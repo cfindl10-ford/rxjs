e25da199de23e0a18e4dfa9a12a5448dda815565
wil pannell <wil.pannell@gmail.com>
2020-07-06 08:20:12 -0700

    squash stopwatch bug

--- src/vanilla/creatingObservables/stopwatch.ts
+++ src/vanilla/creatingObservables/stopwatch.ts
@@ -1,5 +1,5 @@
-import {interval, fromEvent} from 'rxjs'
-import {map, takeUntil, mergeAll} from 'rxjs/operators'
+import {interval, fromEvent, merge} from 'rxjs'
+import {map, takeUntil} from 'rxjs/operators'
 
 const startBtn = document.querySelector('#start-button')
 const stopBtn = document.querySelector('#stop-button')
@@ -7,11 +7,12 @@ const display = document.querySelector<HTMLElement>('.output')
 
 const startBtnClicks = fromEvent(startBtn, 'click')
 const stopBtnClicks = fromEvent(stopBtn, 'click')
+const allClicks = merge(startBtnClicks, stopBtnClicks)
 
 const tenthSeconds = interval(100).pipe(map(tick => tick / 10))
 
 startBtnClicks.subscribe(() => {
   tenthSeconds
-    .pipe(takeUntil(stopBtnClicks))
+    .pipe(takeUntil(allClicks))
     .subscribe(t => (display.innerText = t + 's'))
 })
