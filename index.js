2fcb12a0b1e7fcce84ff5af469b9b0cb7a526b97
wil pannell <wil.pannell@gmail.com>
2020-08-17 09:01:15 -0700

    09-egghead-09-reactive-programming-replace-a-suggestion

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import * as jQuery from 'jquery'
 import {fromEvent, from, of, merge} from 'rxjs'
-import {map, mergeMap, shareReplay, startWith, tap} from 'rxjs/operators'
+import {
+  map,
+  mergeMap,
+  shareReplay,
+  startWith,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators'
+
+const replaceButton1 = document.querySelector('.close1')
+const replaceButton2 = document.querySelector('.close2')
+const replaceButton3 = document.querySelector('.close3')
+
+const replace1ClickStream = fromEvent(replaceButton1, 'click')
+const replace2ClickStream = fromEvent(replaceButton2, 'click')
+const replace3ClickStream = fromEvent(replaceButton3, 'click')
 
 const refresh = document.querySelector('.refresh')
 const refreshClickStream = fromEvent(refresh, 'click')
@@ -16,24 +31,38 @@ const refreshUrlStream = refreshClickStream.pipe(
 const urlStream = merge(startupUrlStream, refreshUrlStream)
 
 const responseStream = urlStream.pipe(
-  tap(() => console.log('request')),
   mergeMap(url => from(jQuery.getJSON(url))),
   shareReplay(1),
 )
 
-function createUserStream(responseStream) {
+// refreshUrlStream:   -------c----------------------->
+// :                   -------N----------------------->
+// urlStream:          u------u----------------------->
+// responseStream:     ---R------R-------------------->
+// replaceClickStream: ---------------x--------------->
+// :                   ---------------R--------------->
+// user1Stream:        N--1---N--1----N--------------->
+
+function getRandom(users) {
+  return users[Math.floor(Math.random() * users.length)]
+}
+
+function createUserStream(responseStream, replaceClickStream) {
   return merge(
+    replaceClickStream.pipe(
+      withLatestFrom(responseStream, (event, users) => getRandom(users)),
+    ),
     responseStream.pipe(
-      map(users => users[Math.floor(Math.random() * users.length)]),
+      map(users => getRandom(users)),
       startWith(null),
     ),
     refreshClickStream.pipe(map(event => null)),
   )
 }
 
-const user1Stream = createUserStream(responseStream)
-const user2Stream = createUserStream(responseStream)
-const user3Stream = createUserStream(responseStream)
+const user1Stream = createUserStream(responseStream, replace1ClickStream)
+const user2Stream = createUserStream(responseStream, replace2ClickStream)
+const user3Stream = createUserStream(responseStream, replace3ClickStream)
 
 user1Stream.subscribe(user => render(user, '.suggestion1'))
 user2Stream.subscribe(user => render(user, '.suggestion2'))
