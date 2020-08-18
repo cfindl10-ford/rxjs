e390e6b8e7e03723429554a807e67719b42d3366
wil pannell <wil.pannell@gmail.com>
2020-08-17 19:27:33 -0700

    02-egghead-stopping-a-stream-with-takeuntil

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 import {fromEvent, interval} from 'rxjs'
-import {switchMapTo} from 'rxjs/operators'
+import {switchMapTo, takeUntil} from 'rxjs/operators'
 
 const log = console.log
 
 // startStream:       ----s----s-s----->
 const startStream = fromEvent(document.querySelector('#start'), 'click')
+// stopStream:       -------x--x---x-->
+const stopStream = fromEvent(document.querySelector('#stop'), 'click')
 
 // intervalStream:    ---0----1---2---->
 const intervalStream = interval(1000)
 
-// startButtonStream:            ----s----s-s----->
+// startStream:                  ----s----s-s----->
 // switchMap(e=>intervalStream): ----i----i-i----->
 // switchMapTo(intervalStream)
 // startIntervalStream:          ----012340101234->
 const startIntervalStream = startStream.pipe(switchMapTo(intervalStream))
 
-startIntervalStream.subscribe(n => log(n))
+// intervalStream:        ---0----1---2---->
+// stopStream:            --------x-------->
+// takeUntil(stopStream):
+// stopIntervalStream   : ---0----1-------->
+const stopIntervalStream = intervalStream.pipe(takeUntil(stopStream))
+
+// startStream:                  ----s-----s-s----->
+// switchMapTo(stopIntervalStream)
+// stopIntervalStream:           --------x-----x-->
+// startIntervalThenStopStream:  ----01234---012-->
+const startIntervalThenStopStream = startStream.pipe(
+  switchMapTo(stopIntervalStream),
+)
+startIntervalThenStopStream.subscribe(n => log(n))
