0cc959dc1878a6557f9c057d24b1ffec60acc870
wil pannell <wil.pannell@gmail.com>
2020-08-17 22:20:07 -0700

    04-egghead-displaying-initial-data-with-startwith

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {fromEvent, interval} from 'rxjs'
-import {scan, switchMapTo, takeUntil} from 'rxjs/operators'
+import {scan, startWith, switchMapTo, takeUntil} from 'rxjs/operators'
 
 const log = console.log
 
@@ -8,14 +8,8 @@ const startStream = fromEvent(document.querySelector('#start'), 'click')
 // stopStream:       -------x--x---x-->
 const stopStream = fromEvent(document.querySelector('#stop'), 'click')
 
-// intervalStream:    ---0----1---2---->
+// intervalStream:        ---0----1---2---->
 const intervalStream = interval(1000)
-
-// startStream:                  ----s----s-s----->
-// switchMap(e=>intervalStream): ----i----i-i----->
-// switchMapTo(intervalStream)
-// startIntervalStream:          ----012340101234->
-const startIntervalStream = startStream.pipe(switchMapTo(intervalStream))
 
 // intervalStream:        ---0----1---2---->
 // stopStream:            --------x-------->
@@ -23,18 +17,14 @@ const startIntervalStream = startStream.pipe(switchMapTo(intervalStream))
 // stopIntervalStream   : ---0----1-------->
 const stopIntervalStream = intervalStream.pipe(takeUntil(stopStream))
 
-// startStream:                  ----s-----s-s----->
-// switchMapTo(stopIntervalStream)
-// stopIntervalStream:           --------x-----x-->
-// startIntervalThenStopStream:  ----01234---012-->
-const startIntervalThenStopStream = startStream.pipe(
-  switchMapTo(stopIntervalStream),
-)
-
-// startIntervalThenStopStream:    ----01234---012-->
-// scan(n=0; n+=1):
-// startStopIntervalContinueCount: ----01234---567-->
-const startStopIntervalContinueCount = startIntervalThenStopStream.pipe(
-  scan(n => n + 1, 0),
-)
-startStopIntervalContinueCount.subscribe(n => log(n))
+// startStream:                     ----s-------s---->
+// switchMapTo(stopIntervalStream): ----01234---567-->
+// startWith(0):                    0---01234---567-->
+// scan(n => n + 1),                0---12345---678-->
+startStream
+  .pipe(
+    switchMapTo(stopIntervalStream),
+    startWith(0),
+    scan(n => n + 1),
+  )
+  .subscribe(n => log(n))
