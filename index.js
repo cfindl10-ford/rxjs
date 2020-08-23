5ad31db49ea8df93d2ab4b64cd3701912c455f53
wil pannell <wil.pannell@gmail.com>
2020-08-22 21:36:36 -0700

    03-07-countdown-timer

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import {interval} from 'rxjs'
+import {scan, mapTo, filter} from 'rxjs/operators'
 
 const log = console.log
+const countdown = document.getElementById('countdown')
+const message = document.getElementById('message')
+
+const counterStream = interval(1000)
+
+counterStream
+  .pipe(
+    mapTo(-1),
+    scan((acc, curr) => acc + curr, 11),
+    filter(count => count >= 0),
+  )
+  .subscribe(count => {
+    countdown.innerHTML = count
+    if (!count) message.innerHTML = 'liftoff'
+  })
