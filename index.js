76822b9f69cd7e717b9e1408073c3b293c40a923
wil pannell <wil.pannell@gmail.com>
2020-08-23 00:23:27 -0700

    04-04-distinctUntilChanged

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+import {from} from 'rxjs'
+import {
+  distinctUntilChanged,
+  map,
+  scan,
+  tap,
+} from 'rxjs/operators'
+
 const log = console.log
+
+const users = [
+  {name: 'Brian', loggedIn: false, token: null},
+  {name: 'Brian', loggedIn: true, token: 'abc'},
+  {name: 'Brian', loggedIn: true, token: '123'},
+]
+
+const state$ = from(users).pipe(
+  scan((acc, curr) => {
+    return {...acc, ...curr}
+  }, {}),
+)
+
+const name$ = state$.pipe(
+  tap(log),
+  map(state => state.name),
+  distinctUntilChanged(),
+)
+
+name$.subscribe(log)
