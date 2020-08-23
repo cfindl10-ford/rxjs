875aaebd3a8911afba5631e677de1f5cdeebd2a3
wil pannell <wil.pannell@gmail.com>
2020-08-22 18:04:47 -0700

    03-04-scroll-progress-bar

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-import {timer} from 'rxjs'
+import {fromEvent} from 'rxjs'
+import {map} from 'rxjs/operators'
 
-const log = console.log
-timer(2000, 500).subscribe(log)
+const percentageProgressFor = ({
+  scrollTop,
+  scrollHeight,
+  clientHeight,
+}) => (scrollTop / (scrollHeight - clientHeight)) * 100
+
+const scrollStream = fromEvent(window, 'scroll')
+
+const progressStream = scrollStream.pipe(
+  map(({target: {scrollingElement}}) =>
+    percentageProgressFor(scrollingElement),
+  ),
+)
+
+const progressElem = document.querySelector('.progress')
+
+progressStream.subscribe(percentage => {
+  progressElem.style.width = `${percentage}%`
+})
